a0ed6de94225083f307d6a36ae6869880210fed7
Ace-Ma <acejingxiaoma@gmail.com>
2019-10-29 18:05:25 -0400

    gitignore and copyright

--- asso/approx.c
+++ asso/approx.c
@@ -1,14 +1,25 @@
 /*
- * asso.c
- *
- * Tries to solve DBP via the association method.
- *
- * Pauli Miettinen
- * 20.6.2005
- *
- * Last modified
- * 19.8.2008
- */
+Copyright (c) 2007 Pauli Miettinen
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 
 #include "approx.h"
 #include "utils.h"
